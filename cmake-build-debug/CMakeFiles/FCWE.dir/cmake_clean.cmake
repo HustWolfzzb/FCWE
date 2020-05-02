bc92c57bb75259e6180d5fd30376fa31c9788475
HustWolfzzb <1184827350@qq.com>
2020-05-02 22:31:26 +0800

    规范化命名2

---
+++ cmake-build-debug/CMakeFiles/FCWE.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FCWE.dir/src/FCWE.c.o"
+  "FCWE"
+  "FCWE.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FCWE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
